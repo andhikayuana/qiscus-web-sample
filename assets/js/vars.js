206d0b03f0ed21a9268457dc2d0894a48629ea84
andhikayuana <andhikayuana@gmail.com>
2019-04-16 10:36:20 +0700

    update

--- assets/js/vars.js
+++ assets/js/vars.js
@@ -1,2 +1,6 @@
-var SDK_APP_ID = 'sampleapp-65ghcsaysse';
+//var SDK_APP_ID = 'sampleapp-65ghcsaysse';
+//var SDK_APP_ID = 'dragongo';
+//var SDK_APP_ID = 'madness-stag'
+var SDK_APP_ID = 'belanja-a-sa4ddetveld'
+//var SDK_APP_ID = 'sdksample'
 var SDK_DASHBOARD_URL = 'https://dashboard-sample.herokuapp.com';
